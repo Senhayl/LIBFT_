b6387696321b55a7da87aadc02b0c70176331b23
Mathis Louron <chevrethis@MacBook-Air-de-Mathis.local>
2025-04-30 11:19:09 +0200

    update  bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: chevrethis <chevrethis@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:14:22 by chevrethis        #+#    #+#             */
-/*   Updated: 2025/04/29 14:55:51 by chevrethis       ###   ########.fr       */
+/*   Updated: 2025/04/30 11:16:33 by chevrethis       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
 
@@ -22,5 +22,4 @@ void	*ft_bzero(void *s, size_t n)
 		((unsigned char *)s)[i] = 0;
 		i++;
 	}
-	return (s);
 }
