b47027607e70fc5e0d2e071899c7d088393b9669
Mathis Louron <chevrethis@MacBook-Air-de-Mathis.local>
2025-05-13 11:49:21 +0200

    13Mayv2

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: chevrethis <chevrethis@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:16:11 by chevrethis        #+#    #+#             */
-/*   Updated: 2025/05/09 12:55:50 by chevrethis       ###   ########.fr       */
+/*   Updated: 2025/05/13 11:48:44 by chevrethis       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@ char	*ft_strdup(const char *s)
 {
 	char	*dup;
 	size_t	len;
+	size_t i;
 
+	i = 0;
 	len = ft_strlen(s) + 1;
 	dup = (char *)malloc(len);
 	if (dup == NULL)
 		return (NULL);
-	ft_memcpy(dup, s, len);
+	while (s[i])
+	{
+		dup[i] = s[i];
+	i++;
+	}
 	return (dup);
 }
  
